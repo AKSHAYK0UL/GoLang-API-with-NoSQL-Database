e51a5d43ca7cdcc000487fd05ab5e4e164c822aa
AKSHAYK0UL <akshaykoul999@gmail.com>
2024-03-19 00:46:31 +0530

    updated

--- main.go
+++ main.go
@@ -27,19 +27,48 @@ type Author struct {
 
 func main() {
 	fmt.Println("Welcome To Golang Api")
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", ServeHome).Methods("GET")
 	r.HandleFunc("/get", Get).Methods("GET")
 	r.HandleFunc("/post", Post).Methods("POST")
 	r.HandleFunc("/search{id}", Search).Methods("GET")
 	r.HandleFunc("/update{id}", Update).Methods("PUT")
+	r.HandleFunc("/del{id}", Delete).Methods("DELETE")
+	r.HandleFunc("/del", DeleteAll).Methods("DELETE")
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "4000"
 	}
-	log.Fatal(http.ListenAndServe(":"+port, r))
-	log.Fatal("Server is Running : ", http.ListenAndServe(":"+port, r))
+	fmt.Println("Server is Running on 192.168.29.1:", port)
+	go keepServerAlive()
+	log.Fatal(http.ListenAndServe("192.168.29.1:"+port, r))
 
+}
+
+// Keep the Server alive
+func keepServerAlive() {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+	for range ticker.C {
+		sendRequest()
+	}
+}
+
+func sendRequest() {
+	fmt.Println("SERVER ALIVE")
+	resp, err := http.Get("https://go-api-with-doc-database.onrender.com")
+	if err != nil {
+		fmt.Printf("Error sending request: %s\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 200 {
+		fmt.Printf("Request sent successfully at %s\n", time.Now().Format(time.RFC3339))
+	} else {
+		fmt.Printf("Failed to send request. Status code: %d\n", resp.StatusCode)
+	}
 }
 
 // home Page
@@ -60,7 +89,7 @@ func Post(w http.ResponseWriter, r *http.Request) {
 	file, _ := os.OpenFile("./courses.json", os.O_RDWR|os.O_CREATE, 0644)
 	json.NewDecoder(file).Decode(&courseData)
 	courseData = append(courseData, newDataFromReq)
-	encoded, _ := json.Marshal(courseData) //convert data in json formate
+	encoded, _ := json.MarshalIndent(courseData, "", "\t") //convert data in json formate
 	file.Seek(0, 0)
 	file.Write(encoded) //write the encoded json in file
 	json.NewEncoder(w).Encode("Data Saved Sucessfully")
@@ -109,7 +138,7 @@ func Update(w http.ResponseWriter, r *http.Request) {
 			json.NewDecoder(r.Body).Decode(&reqData)
 			reqData.CourseId = params["id"]
 			courses = append(courses, reqData)
-			encoded, _ := json.Marshal(courses)
+			encoded, _ := json.MarshalIndent(courses, "", "\t")
 			file.Seek(0, 0)  //set file curser to start
 			file.Truncate(0) //clear the old data
 			file.Write(encoded)
@@ -119,4 +148,35 @@ func Update(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode("No Data Found With This Id")
 	defer file.Close()
+}
+
+// delete one course
+func Delete(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	file, _ := os.OpenFile("./courses.json", os.O_RDWR|os.O_CREATE, 0644)
+	var courses []Course
+	json.NewDecoder(file).Decode(&courses)
+	params := mux.Vars(r)
+	for index, cour := range courses {
+		if cour.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			file.Seek(0, 0)
+			file.Truncate(0)
+			encoded, _ := json.MarshalIndent(courses, "", "\t")
+			file.Write(encoded)
+			json.NewEncoder(w).Encode(courses)
+			return
+
+		}
+	}
+	json.NewEncoder(w).Encode("No Data Found With This Id")
+
+}
+
+// delete all courses
+func DeleteAll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application.json")
+	file, _ := os.OpenFile("./courses.json", os.O_RDWR, 0644)
+	file.Truncate(0)
+	json.NewEncoder(w).Encode("Data Deleted")
 }
